beced6b8012615053cf9d6bc4b0571274b57fa9c
vwsko <vwsko@DESKTOP-7N5I216>
2019-02-28 21:11:23 +0100

    Added top_block_cntrl, removed logic from top_block

---
+++ Music_box.srcs/sources_1/new/top_block_cntrl.vhd
@@ -0,0 +1,96 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+--use IEEE.NUMERIC_STD.ALL;
+
+
+entity top_block_cntrl is
+    Port ( 	clk : in STD_LOGIC;
+    		rst : in STD_LOGIC;
+           	play_btn : in STD_LOGIC;
+           	approved : in STD_LOGIC;
+           	s_tick : in STD_LOGIC;
+           	done : in STD_LOGIC;
+           	play_en : out STD_LOGIC;
+           	rst_cntr : out STD_LOGIC;
+           	inc_cntr : out STD_LOGIC;
+           	wr_en : out STD_LOGIC);
+end top_block_cntrl;
+
+architecture Behavioral of top_block_cntrl is
+
+type state is (wr_state, idle, play);
+    signal state_reg : state;   
+    signal state_next : state := wr_state;
+
+    signal wr_en_next: std_logic;
+	signal play_en_next: std_logic;
+    signal rst_cntr_next: std_logic := '0';
+    signal inc_cntr_next: std_logic;
+
+begin
+
+
+-- Register
+    process (clk, rst)
+    begin
+        if (rst = '1') then
+            state_reg <= wr_state;
+            wr_en <= '0';  
+        elsif (rising_edge(clk)) then
+            state_reg <= state_next;
+            wr_en <= wr_en_next;
+            play_en <= play_en_next;
+            rst_cntr <= rst_cntr_next;
+            inc_cntr <= inc_cntr_next;         
+        end if;
+    end process;
+    
+    -- Next-state
+    process (state_reg, play_btn, approved, s_tick, done)
+    begin
+
+    	state_next <= state_reg;
+
+        case (state_reg) is
+            when wr_state =>
+                
+                wr_en_next <= '0';
+                inc_cntr_next <= '0';
+                
+                if (done = '0') then 
+                    if (approved ='1') then
+                        wr_en_next <= '1';
+                        inc_cntr_next <= '1';
+                    --elsif unsigned(r_data) = x"20" then
+                    --    state_next <= idle;
+                    end if;
+                else
+                    state_next <= idle;
+                end if;
+            when idle =>
+                play_en_next <= '0';
+                
+                if (play_btn='1') then--rising_edge(play_btn)) then
+                    play_en_next <= '1';
+                    rst_cntr_next <= '1';
+                    state_next <= play;
+                end if;
+            when others =>
+                rst_cntr_next <= '0';
+                inc_cntr_next <= '0';
+                
+                if (done = '0') then
+                    if (s_tick = '1') then
+                        inc_cntr_next <= '1';                        
+                    end if;
+                else
+                    state_next <= idle;
+                    play_en_next <= '0';
+                end if;
+        end case;
+    end process;    
+    
+    --rx_done_out <= rx_done;
+    --r_data_out <= r_data;           
+
+end Behavioral;
